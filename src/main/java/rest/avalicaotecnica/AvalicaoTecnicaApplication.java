83a2a3b6f8c34f9a169ccbd8e21c9d1f2db03571
Cleiton.Oliveira <cleiton.oliveira>
2021-02-08 10:27:11 -0300

    commit inicial dos arquivos do projetos

---
+++ src/main/java/rest/avalicaotecnica/AvalicaoTecnicaApplication.java
@@ -0,0 +1,13 @@
+package rest.avalicaotecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalicaoTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalicaoTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rest/avalicaotecnica/AvalicaoTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.avalicaotecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalicaoTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
